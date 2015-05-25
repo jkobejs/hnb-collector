1ac4f12d171af79306d1cc0825a10e232c8a1941
jgrgurica <jgrgurica@extensionengine.com>
2015-05-25 18:55:53 +0200

    rearange files, export collectData function, set repository

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 'use strict';
 
-var collectData = require('./utils/collector').collectData;
+var streams = require('./lib/streams'),
+    es      = require('event-stream');
 
-var stream = collectData(null, new Date(1997, 9, 3));
+function collectData(startDate, endDate, dateFormat) {
+    startDate = startDate || new Date(1997, 9, 1); // 1.10.1997
+    endDate = endDate || new Date(); // today
+    dateFormat = dateFormat || 'DDMMYY';
 
-stream.pipe(process.stdout);
+    var url = 'http://www.hnb.hr/tecajn/f%s.dat';
+    var regex   = /\d{3}(\w{3})(\d{3})\s*(\d+,\d+)\s*(\d+,\d+)\s*(\d+,\d+)/;
+
+    return streams.dateProducer(startDate, endDate, dateFormat)
+        .pipe(streams.mapResponseBodies(url))
+        .pipe(es.split())
+        .pipe(streams.extract(regex));
+}
+
+exports.collectData = collectData;
